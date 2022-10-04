3ac01934f2d03d698779f01d4173b5b62eae4cb6
mohit5743 <82862354+mohit5743@users.noreply.github.com>
2022-10-04 13:22:32 +0530

    init

--- src/mongoose/connect.js
+++ src/mongoose/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const db = ()=>{
-        mongoose.connect('mongodb+srv://mohit:Mchopra123@cluster0.36k0lak.mongodb.net/?retryWrites=true&w=majority', {
+        mongoose.connect('mongodb+srv://mohit:Mchopra123@cluster0.36k0lak.mongodb.net/Books?retryWrites=true&w=majority', {
         useCreateIndex: "true",
         useNewUrlParser: "true",
         useUnifiedTopology: "true",
